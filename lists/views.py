098b68c8474de1ff0db148595d7449224a740d73
Carl Brubaker <CrazyCarl@tgfdsunkh54fhjlkhgkfdhu.local>
2018-06-21 20:45:56 -0400

    first push to the world

--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render, redirect
-from lists.models import Item
-
-# Create your views here.
+from django.shortcuts import redirect, render
+from lists.models import Item, List
 
 def home_page(request):
-    if request.POST:
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+    return render(request, 'home.html')
     
-    items = Item.objects.all() 
-    return render(request, 'home.html', {'items': items})
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.pk}/')
+    
+def view_list(request, pk):
+    list_ = List.objects.get(id=pk)
+    return render(request, 'list.html', {'list': list_})
+
+def add_item(request, pk):
+    list_ = List.objects.get(id=pk)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.pk}/')
