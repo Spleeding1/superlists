b66abfd6976a75ccc63bc88484dc4da5f3a2c85e
Carl Brubaker <CrazyCarl@tgfdsunkh54fhjlkhgkfdhu.local>
2018-06-27 22:44:46 -0400

    switched debug if statement

--- superlists/settings.py
+++ superlists/settings.py
@@ -16,14 +16,14 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-if 'DJANGO_DEBUG_TRUE' in os.environ:
-    DEBUG = True
-    SECRET_KEY = 'insecure-key-for-dev'
-    ALLOWED_HOSTS = []
-else:
+if 'DJANGO_DEBUG_FALSE' in os.environ:
     DEBUG = False
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
     ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEBUG = True
+    SECRET_KEY = 'insecure-key-for-dev'
+    ALLOWED_HOSTS = []
     
 
 
